a69ed0d68b1509b04a000a5779fe24593603f2a7
yondikavl <yondikaviolanda@gmail.com>
2022-06-26 20:24:13 +0700

    edit view/admin/index.php

--- view/admin/index.php
+++ view/admin/index.php
@@ -10,11 +10,13 @@ include'../403/admin.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard-Admin</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
+  
   <body>
 
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="?hal=profile_admin"><?=$NamaAdmin?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,7 +34,9 @@ include'../403/admin.php';
               <a class="nav-link" href="?hal=pengguna">Pengguna</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../../system/system-logout.php">Logout</a>
+              <a class="btn-logout" href="../../system/system-logout.php">
+                <button type="button" class="btn btn-success">Logout</button>
+              </a>
             </li>
           </ul>
         </div>
@@ -42,6 +46,20 @@ include'../403/admin.php';
     <?php 
     include'../../koneksi/controller.php'; ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- Style CSS -->
+    <style>
+      .navbar {
+        background-color: #19123B;
+      }
+      .btn-logout {
+        margin-right: 20px;
+        margin-left: 1000px;
+      }
+    </style>
+
   </body>
+
+  <!-- Script JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </html>
